802de0021716d578ed6aa5f2613e1bfb13269ae4
Michael Papworth <michaelpapworth@users.noreply.github.com>
2013-12-05 16:49:22 +0000

    Create navigate.debug.js

---
+++ navigate.debug.js
@@ -0,0 +1,39 @@
+jQuery.extend({
+	'navigateSettings': {
+		api: '',
+		endpoints: {}
+	},
+	'navigateSetup': function (settings) {
+		jQuery.extend(jQuery.navigateSettings, settings);
+	},
+	'navigate': function (method) {
+		var methods = {
+			goTo: function (url) {
+				jQuery('<a href="' + url + '"></a>')[0].click();
+			},
+			to: function (identifier, params) {
+				var pattern = jQuery.navigateSettings.endpoints[identifier];
+				var url = resolve(pattern, params)
+				methods.goTo(url);
+			},
+			api: function () {
+				var result = jQuery.navigateSettings['api'];
+				for (var index in arguments) {
+					result += arguments[index] + '/';
+				}
+				return result;
+			}
+		};
+
+		var resolve = function (pattern, params) {
+			return pattern.replace(/{([a-zA-Z1-9]+)}/g, function (match, number) {
+				return typeof params[number] != 'undefined' ? params[number] : '';
+			});
+		};
+
+		if (methods[method])
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		else
+			jQuery.error('Method ' + method + ' does not exist on jQuery.navigate');
+	}
+});
