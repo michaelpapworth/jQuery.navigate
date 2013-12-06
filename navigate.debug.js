0aafbf9c155c7b025efe8b9e1c42a8e916e8d0d6
Michael Papworth <michaelpapworth@users.noreply.github.com>
2013-12-06 10:55:22 +0000

    Update navigate.debug.js
    Provided code comments for clarity
    Tidied up some obvious inconsistencies

--- navigate.debug.js
+++ navigate.debug.js
@@ -1,43 +1,63 @@
 jQuery.extend({
+	// The settings singleton object.
 	'navigateSettings': {
 		api: '',
 		endpoints: {}
 	},
+	
+	// Setup to be called once to initialise the plugin.
 	'navigateSetup': function (settings) {
 		jQuery.extend(jQuery.navigateSettings, settings);
 	},
+	
+	// Defines the plugin.
 	'navigate': function (method) {
 		var methods = {
-			goTo: function (url) {
+			// Navigate directly to the URL provided.
+			'goTo': function (url) {
+				// Ensure the referer is passed on the request.
 				jQuery('<a href="' + url + '"></a>')[0].click();
 			},
-			to: function (identifier, params) {
+			
+			// Uses the params object to build the URL specified by the identifier, then navigates to it.
+			'to': function (identifier, params) {
 				var pattern = jQuery.navigateSettings.endpoints[identifier];
 				var url = resolve(pattern, params)
 				methods.goTo(url);
 			},
-			api: function () {
+			
+			// Provide arguments to build a URL based pn the API settings.
+			'api': function () {
 				var result = jQuery.navigateSettings['api'];
+				
+				// Append each argument to the API URL, delimited with '/'.
 				for (var index in arguments) {
 					result += arguments[index] + '/';
 				}
+				
 				return result;
 			},
-			url: function (identifier, params) {
+			
+			// Returns the URL specified by the identifier, using the params object to build the it.
+			'url': function (identifier, params) {
 				var pattern = jQuery.navigateSettings.endpoints[identifier];
 				return resolve(pattern, params)
 			},
 		};
 
+		// Replaces placeholders contained by the pattern with the values of matching properties on the params object
 		var resolve = function (pattern, params) {
+			if (!params) return pattern;
+			
 			return pattern.replace(/{([a-zA-Z1-9]+)}/g, function (match, number) {
 				return typeof params[number] != 'undefined' ? params[number] : '';
 			});
 		};
 
-		if (methods[method])
+		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		else
+		} else {
 			jQuery.error('Method ' + method + ' does not exist on jQuery.navigate');
+		}
 	}
 });
