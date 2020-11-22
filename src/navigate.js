33f40778c99687c8ea425584233e8591c0a5cd0e
Dominic Sidiropoulos <dominick.sidiropoulos@gmail.com>
2020-11-22 17:03:23 +0200

    chore (JQN-00003-eslint): finetune eslintrc.js
    Signed-off-by: Dominic Sidiropoulos <dominick.sidiropoulos@gmail.com>

--- src/navigate.js
+++ src/navigate.js
@@ -7,70 +7,78 @@
  */
 
 (function ($) {
-  $.extend({
-    // The settings singleton object.
-    navigateSettings: {
-      api: '',
-      endpoints: {},
-    },
-
-    // Setup to be called once to initialise the plugin.
-    navigateSetup: function (settings) {
-      $.extend($.navigateSettings, settings);
-    },
-
-    // Defines the plugin.
-    navigate: function (method) {
-      var methods = {
-        // Navigate directly to the URL provided.
-        goTo: function (url) {
-          // Ensure the referer is passed on the request.
-          $('<a></a>').attr('href', url).get(0).click();
+    $.extend({
+        // The settings singleton object.
+        navigateSettings: {
+            api: '',
+            endpoints: {},
         },
 
-        // Uses the params object to build the URL specified by the identifier, then navigates to it.
-        to: function (identifier, params) {
-          var pattern = $.navigateSettings.endpoints[identifier];
-          var url = resolve(pattern, params);
-          methods.goTo(url);
+        // Setup to be called once to initialise the plugin.
+        navigateSetup: function (settings) {
+            $.extend($.navigateSettings, settings);
         },
 
-        // Provide arguments to build a URL based pn the API settings.
-        api: function () {
-          var result = $.navigateSettings['api'];
+        // Defines the plugin.
+        navigate: function (method) {
+            var methods = {
+                // Navigate directly to the URL provided.
+                goTo: function (url) {
+                    // Ensure the referer is passed on the request.
+                    $('<a></a>')
+                        .attr('href', url)
+                        .get(0)
+                        .click();
+                },
 
-          // Append each argument to the API URL, delimited with '/'.
-          for (var index in arguments) {
-            result += arguments[index] + '/';
-          }
+                // Uses the params object to build the URL specified by the identifier, then navigates to it.
+                to: function (identifier, params) {
+                    var pattern = $.navigateSettings.endpoints[identifier];
+                    var url = resolve(pattern, params);
+                    methods.goTo(url);
+                },
 
-          return result;
+                // Provide arguments to build a URL based pn the API settings.
+                api: function () {
+                    var result = $.navigateSettings['api'];
+
+                    // Append each argument to the API URL, delimited with '/'.
+                    for (var index in arguments) {
+                        result += arguments[index] + '/';
+                    }
+
+                    return result;
+                },
+
+                // Returns the URL specified by the identifier, using the params object to build the it.
+                url: function (identifier, params) {
+                    var pattern = $.navigateSettings.endpoints[identifier];
+                    return resolve(pattern, params);
+                },
+            };
+
+            // Replaces placeholders contained by the pattern with the values of matching properties on the params object
+            var resolve = function (pattern, params) {
+                if (!params) return pattern;
+
+                return pattern.replace(
+                    /{([a-zA-Z1-9]+)}/g,
+                    function (match, number) {
+                        return typeof params[number] != 'undefined'
+                            ? params[number]
+                            : '';
+                    }
+                );
+            };
+
+            if (methods[method]) {
+                return methods[method].apply(
+                    this,
+                    Array.prototype.slice.call(arguments, 1)
+                );
+            } else {
+                $.error('Method ' + method + ' does not exist on $.navigate');
+            }
         },
-
-        // Returns the URL specified by the identifier, using the params object to build the it.
-        url: function (identifier, params) {
-          var pattern = $.navigateSettings.endpoints[identifier];
-          return resolve(pattern, params);
-        },
-      };
-
-      // Replaces placeholders contained by the pattern with the values of matching properties on the params object
-      var resolve = function (pattern, params) {
-        if (!params) return pattern;
-
-        return pattern.replace(/{([a-zA-Z1-9]+)}/g, function (match, number) {
-          return typeof params[number] != 'undefined' ? params[number] : '';
-        });
-      };
-
-      if (methods[method]) {
-        return methods[method].apply(
-          this,
-          Array.prototype.slice.call(arguments, 1)
-        );
-      } else {
-        $.error('Method ' + method + ' does not exist on $.navigate');
-      }
-    },
-  });
+    });
 })(window.jQuery);
